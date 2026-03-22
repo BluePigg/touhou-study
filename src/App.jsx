f57f1e714d4099e4631958d17157612f2872474a
bluepig <philipark0903@gmail.com>
2026-03-22 16:34:20 +0900

    UI 틀 잡기 + @
    필요없는 파일 제거 및 아이콘, 검색창, 랜덤 버튼, 프로필 이미지 추가

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import React from "react";
+import { Button, TextField } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="NavBar">
+        <img
+          src={`${process.env.PUBLIC_URL}/images/fumo.png`}
+          className="fumo"
+        ></img>
+        <TextField
+          variant="filled"
+          className="SearchBar"
+          label="동캐 이름 입력 (ex: 하쿠레이레이무)"
+        ></TextField>
+        <Button variant="contained" className="Random" color="primary">
+          랜덤 동캐
+        </Button>
+      </div>
+      <div className="Main">
+        <img
+          src="https://i.namu.wiki/i/wgzMvFitt_Ccx3R_5TSQgvN94SCkeLH326fQcjGTVxfgnul34UVJ6Sp5Skhz_8HftWrSf2OZjNQp1NjiF2wNjYT2Le9Atm38_Hj0MQFijIC2uffrZkjR_MncTxRDDUwAydIafQvSJDXqwFH-Suz5Hg.webp"
+          className={`pfImage`}
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
